9e754b8ffc37a7883c50a254c63e016f11bb0335
taichiH <higashide@jsk.imi.i.u-tokyo.ac.jp>
2019-07-02 21:39:24 +0900

    rename yolo3_visualizer

--- ros/src/computing/perception/detection/trafficlight_recognizer/scripts/yolo3_visualizer.py
+++ ros/src/computing/perception/detection/trafficlight_recognizer/scripts/yolo3_visualizer.py
@@ -13,23 +13,26 @@ class Yolo3Vizualizer():
 
     def __init__(self):
         self.bridge = cv_bridge.CvBridge()
-        self.width = 1920
-        self.height = 1200
         self.image = None
+        self.check_callback = False
 
         self.image_pub = rospy.Publisher(
-            '/yolo3_vizualizer/output', Image, queue_size=1)
+            '~output', Image, queue_size=1)
 
         rospy.Subscriber(
-            '/detection/image_detector/croped_objects', DetectedObjectArray, self.callback)
+            '~input_detected_boxes', DetectedObjectArray, self.callback)
         rospy.Subscriber(
-            '/image_raw', Image, self.image_callback)
+            '~input_image', Image, self.image_callback)
 
     def image_callback(self, msg):
+        self.check_callback = True
         br = self.bridge
         self.image = br.imgmsg_to_cv2(msg, desired_encoding='bgr8')
 
     def callback(self, msg):
+        if not self.check_callback:
+            return
+
         br = self.bridge
         img = copy.copy(self.image)
         for obj in msg.objects:
