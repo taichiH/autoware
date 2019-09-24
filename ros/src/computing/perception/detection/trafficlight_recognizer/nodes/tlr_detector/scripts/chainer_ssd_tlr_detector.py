fc80fb8ce4838737f4334aef5c5c31ed687eff12
taichiH <higashide@jsk.imi.i.u-tokyo.ac.jp>
2019-09-25 06:53:41 +0900

    fix tlr_ssd_detector bug

--- ros/src/computing/perception/detection/trafficlight_recognizer/nodes/tlr_detector/scripts/chainer_ssd_tlr_detector.py
+++ ros/src/computing/perception/detection/trafficlight_recognizer/nodes/tlr_detector/scripts/chainer_ssd_tlr_detector.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import copy
+
 from cv_bridge import CvBridge
+import cv2
 import numpy as np
 
 import rospy
@@ -36,6 +39,8 @@ class TLRSSDDetector():
 
         self.stamped_roi_pub = rospy.Publisher(
             '~output_stamped_roi', StampedRoi, queue_size=1)
+        self.debug_image_pub = rospy.Publisher(
+            '~output_debug_image', Image, queue_size=1)
 
         queue_size = rospy.get_param('~queue_size', 100)
         sub_image = message_filters.Subscriber(
@@ -69,10 +74,22 @@ class TLRSSDDetector():
 
         return lt, rb
 
+    def plt2cv(self, img):
+        fig = plt.gcf()
+        fig.canvas.draw()
+        w, h = fig.canvas.get_width_height()
+        img = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8)
+        fig.clf()
+        img.shape = (h, w, 3)
+        plt.close()
+
+        return img
+
     def callback(self, image_msg, stamped_roi_msg):
         try:
             # transform image to RGB, float, CHW
             img = self.cv_bridge.imgmsg_to_cv2(image_msg, desired_encoding="rgb8")
+            display_img = copy.copy(img)
             img = np.asarray(img, dtype=np.float32)
             img = img.transpose(2, 0, 1)  # (H, W, C) -> (C, H, W)
         except Exception as e:
@@ -85,21 +102,31 @@ class TLRSSDDetector():
         stamped_roi = StampedRoi()
         for i, roi in enumerate(stamped_roi_msg.roi_array):
             signal = stamped_roi_msg.signals[i]
-
             croped_img = img[:,
                              roi.y_offset : roi.y_offset + roi.height,
                              roi.x_offset : roi.x_offset + roi.width]
 
+            lt = (int(roi.x_offset), int(roi.y_offset))
+            rb = (int(roi.x_offset + roi.width), int(roi.y_offset + roi.height))
+            cv2.rectangle(display_img, lt, rb, (0,255,0), 2)
+
             bboxes, labels, scores = self.model.predict([croped_img])
             bboxes, labels, scores = bboxes[0], labels[0], scores[0]
+
+            if len(bboxes) == 0:
+                continue
 
             # get highest score bbox
             bbox = bboxes[np.argmax(scores)]
 
-            lt = (bbox[1], bbox[0])
-            rb = (bbox[3] - bbox[1], bbox[2] - bbox[0])
-            size = (croped_img.shape[-1], croped_img.shape[-2])
-            lt, rb = self.fit_in_frame(lt, rb, size)
+            box_lt = [int(round(lt[0] + bbox[1])),
+                      int(round(lt[1] + bbox[0]))]
+            box_rb = [int(round(lt[0] + bbox[3])),
+                      int(round(lt[1] + bbox[2]))]
+
+            size = (img.shape[-1], img.shape[-2])
+            box_lt, box_rb = self.fit_in_frame(box_lt, box_rb, size)
+            cv2.rectangle(display_img, tuple(box_lt), tuple(box_rb), (255,0, 0), 2)
 
             stamped_roi.roi_array.append(
                 RegionOfInterest(x_offset=lt[0],
@@ -112,6 +139,10 @@ class TLRSSDDetector():
 
         stamped_roi.header = stamped_roi_msg.header
         self.stamped_roi_pub.publish(stamped_roi)
+
+        debug_image_msg = self.cv_bridge.cv2_to_imgmsg(display_img, "rgb8")
+        debug_image_msg.header = image_msg.header
+        self.debug_image_pub.publish(debug_image_msg)
 
 
 if __name__ == '__main__':
