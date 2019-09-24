b9bf48823e677b76c1dee0896741ea695576cc06
taichiH <higashide@jsk.imi.i.u-tokyo.ac.jp>
2019-09-25 04:33:00 +0900

    add tlr_ssd_detector test

--- ros/src/computing/perception/detection/trafficlight_recognizer/nodes/tlr_detector/scripts/chainer_ssd_tlr_detector.py
+++ ros/src/computing/perception/detection/trafficlight_recognizer/nodes/tlr_detector/scripts/chainer_ssd_tlr_detector.py
@@ -3,62 +3,55 @@
 
 from cv_bridge import CvBridge
 import numpy as np
+
 import rospy
+import message_filters
 
 from sensor_msgs.msg import Image
 from autoware_msgs.msg import StampedRoi
-import message_filters
+from sensor_msgs.msg import RegionOfInterest
 
 import chainer
 from chainercv.links import SSD300
-from chainercv.links import SSD512
-from chainercv.visualizations import vis_bbox
 
-
-class SSDObjectDetector():
+class TLRSSDDetector():
 
     def __init__(self):
-        super(SSDObjectDetector, self).__init__()
+        self.approximate_sync = rospy.get_param('~approximate_sync', True)
         self.gpu = rospy.get_param("~gpu", -1)
         self.cv_bridge = CvBridge()
 
         # load model
         self.label_names = ['trafficlight']
         rospy.loginfo("Loaded %d labels" % len(self.label_names))
-
         model_path = rospy.get_param("~model_path", None)
-        model_name = rospy.get_param('~model', 'ssd300')
-        if model_name == 'ssd300':
-            model_class = SSD300
-        elif model_name == 'ssd512':
-            model_class = SSD512
-        else:
-            rospy.logerr('Unsupported ~model: {}'.format(model_name))
-
+        model_class = SSD300
         self.model = model_class(n_fg_class=len(self.label_names),
                                  pretrained_model=model_path)
-
         if self.gpu >= 0:
             chainer.cuda.get_device_from_id(self.gpu).use()
             self.model.to_gpu()
-
         rospy.loginfo("Loaded model: %s" % model_path)
+
 
         self.stamped_roi_pub = rospy.Publisher(
             '~output_stamped_roi', StampedRoi, queue_size=1)
 
+        queue_size = rospy.get_param('~queue_size', 100)
         sub_image = message_filters.Subscriber(
-            '~input_image', Image, queue_size=1, buff_size=2**24)
+            '~input_image', Image, queue_size=queue_size)
         sub_stamped_roi = message_filters.Subscriber(
-            '~input_stamp_roi', StampedRoi, queue_size=1, buff_size=2**24)
+            '~input_stamped_roi', StampedRoi, queue_size=queue_size)
+
         self.subs = [sub_image, sub_stamped_roi]
-        if rospy.get_param('~approximate_sync', True):
+        if self.approximate_sync:
             slop = rospy.get_param('~slop', 0.1)
             sync = message_filters.ApproximateTimeSynchronizer(
                 fs=self.subs, queue_size=queue_size, slop=slop)
         else:
             sync = message_filters.TimeSynchronizer(
                 fs=self.subs, queue_size=queue_size)
+
         sync.registerCallback(self.callback)
 
 
@@ -76,33 +69,32 @@ class SSDObjectDetector():
             chainer.cuda.get_device_from_id(self.gpu).use()
 
         stamped_roi = StampedRoi()
-        for projcted_roi in stamped_roi_msg.roi_array:
+        for i, roi in enumerate(stamped_roi_msg.roi_array):
+            signal = stamped_roi_msg.signals[i]
 
-            croped_img = img[projected_roi.y_offset:
-                             projected_roi.y_offset + projected_roi.height,
-                             projected_roi.x_offset:
-                             projected_roi.x_offset + projected_roi.width]
+            croped_img = img[:,
+                             roi.y_offset : roi.y_offset + roi.height,
+                             roi.x_offset : roi.x_offset + roi.width]
 
             bboxes, labels, scores = self.model.predict([croped_img])
             bboxes, labels, scores = bboxes[0], labels[0], scores[0]
 
-            stamped_roi = StampedRoi()
-            prev_score = 0
-            for bbox, scores in zip(bboxes, scores):
-                if scores > prev_scores:
-                    best_match_box = bbox
-                    prev_scoers = scores
+            # get highest score bbox
+            bbox = bboxes[np.argmax(scores)]
 
-            roi = RegionOfInterest(x_offset = best_match_bbox[1],
-                                   y_offset = best_match_bbox[0],
-                                   width = best_match_bbox[3] - best_match_bbox[1],
-                                   height = best_match_bbox[2] - best_match_bbox[0])
-            stamped_roi.roi_array.append(roi)
+            stamped_roi.roi_array.append(
+                RegionOfInterest(
+                    x_offset = bbox[1],
+                    y_offset = bbox[0],
+                    width = bbox[3] - bbox[1],
+                    height = bbox[2] - bbox[0]))
+            stamped_roi.signals.append(signal)
 
+        stamped_roi.header = stamped_roi_msg.header
         self.stamped_roi_pub.publish(stamped_roi)
 
 
 if __name__ == '__main__':
-    rospy.init_node("ssd_object_detector")
-    ssd = SSDObjectDetector()
+    rospy.init_node("tlr_ssd_detector")
+    ssd = TLRSSDDetector()
     rospy.spin()
---
+++ ros/src/computing/perception/detection/trafficlight_recognizer/scripts/dummy_stamped_roi_publisher.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import rospy
+
+from sensor_msgs.msg import RegionOfInterest
+from autoware_msgs.msg import StampedRoi
+
+class DummyStampedRoiPublisher(object):
+
+    def __init__(self):
+        self.seq = 0
+        self.frame_id = "base_link"
+        self.dynamic_signal = 1
+        self.pub = rospy.Publisher('~output', StampedRoi, queue_size=1)
+        rate = rospy.get_param('~rate', 1)
+        self.timer = rospy.Timer(rospy.Duration(1. / rate), self.publish)
+
+    def publish(self, event):
+        stamped_roi_msg = StampedRoi()
+        stamped_roi_msg.header.seq = self.seq
+        stamped_roi_msg.header.frame_id = self.frame_id
+        stamped_roi_msg.header.stamp = event.current_real
+
+        for i in range(1):
+            roi = RegionOfInterest()
+            roi.x_offset = 200
+            roi.y_offset = 200
+            roi.width = 200
+            roi.height = 200
+
+            stamped_roi_msg.signals.append(1)
+            stamped_roi_msg.roi_array.append(roi)
+
+        self.pub.publish(stamped_roi_msg)
+
+if __name__ == '__main__':
+    rospy.init_node('dummy_stamped_roi_publisher')
+    DummyStampedRoiPublisher()
+    rospy.spin()
